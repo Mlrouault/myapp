ef928175dfae72a2c4f85e34b9ca931d00a49807
lvl25chad <lvl26chad@gmail.com>
2022-10-03 13:18:32 -0600

    Created models and Controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/' => 'users#index'
+  # post '/sessions' => 'sessions#create'
+  post '/users' => 'users#create'
+  get '/dashboard' => 'clients#index'
 end
