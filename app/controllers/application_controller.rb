7ce7b1234f1debb6b821906c14b5f1721c20fb25
lvl25chad <lvl26chad@gmail.com>
2022-10-04 22:38:06 -0600

    Added user based item table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
+require 'sessions_helper'
+
+include SessionsHelper
+
+
+
 class ApplicationController < ActionController::Base
+
+    private
+        def logged_in_user
+            unless logged_in?
+            store_location
+            flash[:danger] = "Please log in."
+            redirect_to login_url
+            end
+        end
 end
